e8b0cd390c375665b33686bedd3cbdee52939dff
Vlad Konkin <sodii@S1mple-Air.N4>
2022-01-07 17:31:21 +1000

    Calc

---
+++ Documents/calc/calc.js
@@ -0,0 +1,89 @@
+let a = '';// first number
+let b = '';//second number
+let sign = '';//math sign
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X','/'];
+
+//display
+const out = document.querySelector('.calc__screen p');
+
+function clearAll () {
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    out.textContent = 0;
+}
+
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+    //no button is pushed
+    if(!event.target.classList.contains('btn')) return;
+    // ac button is pushed
+    if(event.target.classList.contains('ac')) return;
+
+    out.textContent = '';
+    //get pushed button
+    const key = event.target.textContent;
+
+    //if button 0-9 or . pushed
+    if (digit.includes(key)) {
+        if (b === '' && sign === '') {
+            a += key;
+
+        out.textContent = a;
+        }
+        else if (a !== '' && b !=='' && finish) {
+            b = key;
+            finish = false;
+            out.textContent = b;
+        }
+        else {
+            b += key;
+            out.textContent = b;
+        }
+        console.log(a, b , sign);
+        return;
+    }
+
+    //if sign button pushed
+    if (action.includes(key)) {
+        sign = key;
+        out.textContent = sign;
+        console.log(a, b, sign);
+        return;
+    }
+
+    //if = is pushed
+    if (key === '=') {
+        if (b ==='') b = a;
+        switch (sign) {
+            case "+":
+                a = (+a) + (+b);
+                break;
+            case "-":
+                a = (+a) - (+b);
+                break;
+            case "X":
+                a = (+a) * (+b);
+                break;
+            case "/":
+                if (b === '0') {
+                    out.textContent = 'Error';
+                    a = '';
+                    b = '';
+                    sign = '';
+                    return;
+                }
+                a = (+a) / (+b);
+                break;
+        }
+        finish = true;
+        out.textContent = a;
+        console.table(a, b, sign);
+    }
+
+}
